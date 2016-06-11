277aca3c906ebd8625830037ad490258beae801f
Sachin G <sachin.gavas99@gmail.com>
2016-06-11 23:26:04 +0530

    removed CI

--- application/views/js.php
+++
@@ -1,8 +0,0 @@
-
-<form id="frmInvoice" role="form" action="<?php echo $this->config->base_url(); ?>js" method="post">
-<input type="text" name="name" value="" />
-<input type="text" name="fld1" value="" />
-<input type="text" name="fld2" value="" />
-<input type="submit">
-</form>
-
